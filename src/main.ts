7ed7b1369ba4f129932bcd0fbfdc9fab34ffe5f0
Nguyen-Duc-Trong <trongnguyen160902@gmail.com>
2026-05-01 17:02:37 +0700

    feat: initialize NestJS application with session management, global validation pipes, and API prefix

--- src/main.ts
+++ src/main.ts
@@ -37,6 +37,6 @@ async function bootstrap() {
 
 	app.setGlobalPrefix('api/wallet')
 
-	await app.listen(process.env.PORT ?? 3000);
+	await app.listen(process.env.PORT ?? 3000, '0.0.0.0');
 }
 bootstrap();
